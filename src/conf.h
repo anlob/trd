b85c6fc0cccb6f1cb3ef7abaedb54ed529e4423b
anlob <andreas.lobbes@gmail.com>
2018-06-16 19:37:43 +0200

    doxygen hdr + copyright info

--- src/conf.h
+++ src/conf.h
@@ -1,3 +1,12 @@
+/** \file conf.h
+ * Config stuff.
+ *
+ * \author Andreas Lobbes, andreas.lobbes@gmail.com
+ * \date 2018
+ * \copyright MS-RSL
+ * \see LICENSES at top level directory
+ * \see https://referencesource.microsoft.com/license.html
+ */
 #ifndef CONF_H_INCLUDED
 #define CONF_H_INCLUDED
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+/** \file main.cpp
+ *
+ * \author Andreas Lobbes, andreas.lobbes@gmail.com
+ * \date 2018
+ * \copyright MS-RSL
+ * \see LICENSES at top level directory
+ * \see https://referencesource.microsoft.com/license.html
+ */
 #include "conf.h"
 
 using namespace std;
